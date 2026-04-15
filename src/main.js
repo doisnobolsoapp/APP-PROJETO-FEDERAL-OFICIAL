46253f3afa9d8c172cbdf655a00b55b65c0a5f68
doisnobolsoapp <willrsiqueira.estudos@gmail.com>
2026-04-15 16:29:27 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,2 @@
 import './style.css';
 import './app.js';
-
-console.log("🚀 MAIN INICIADO");
-
-// SERVICE WORKER
-if ('serviceWorker' in navigator && location.hostname !== "localhost") {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('✅ SW registrado:', reg))
-      .catch(err => console.log('❌ SW erro:', err));
-  });
-}
